d48b113b372a4e7b6792f85f60997df388f71187
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-04-01 18:37:34 +0800

    add Response facade to controller

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Lesson;
+use Response;
+
 class LessonsController extends Controller
 {
     public function index(){
-    	return Lesson::all();	//really bad practice	
+    	$lessons = Lesson::all();	//really bad practice	
+    	return Response::json([
+    		'data' => $lessons->toArray()
+    	], 200);
     }
 }
