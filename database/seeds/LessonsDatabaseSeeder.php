6c5334f673dc3cb15abe5ecbc8aa49f0c7cae6da
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-04-01 17:19:35 +0800

    add blank Lessons Controller, make seeds 30, add lessons resource

--- database/seeds/LessonsDatabaseSeeder.php
+++ database/seeds/LessonsDatabaseSeeder.php
@@ -9,7 +9,7 @@ class LessonsDatabaseSeeder extends Seeder {
 	{
 		$faker = Faker::create();
 
-		foreach (range(1,10) as $index) {
+		foreach (range(1,30) as $index) {
 			// Lesson::create([
 			// 	'title' => $faker->sentence(5),
 			// 	'body' => $faker->paragraph(4)
