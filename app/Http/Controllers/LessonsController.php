f3d9c4dfafd1883946c793a923d82d26bc018cca
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-04-01 18:01:18 +0800

    use Lesson model

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Lesson;
 class LessonsController extends Controller
 {
-    //
+    public function index(){
+    	return Lesson::all();	//really bad practice	
+    }
 }
